6a662e7c084b1f5ee83e2a967e88041dd5c3815e
Joshix <joshix.dev@mailbox.org>
2021-06-03 23:32:16 +0200

    fix script

--- restart.sh
+++ restart.sh
@@ -2,8 +2,10 @@
 
 git pull
 
-[ ! -f /etc/systemd/system/an-website.service ] && ln -s an-website.service /etc/systemd/system/an-website.service && systemctl enable an-website.service
+cp an-website.service /etc/systemd/system/an-website.service
 
-[ ! -f /usr/lib/an-website/main.py ] && ln -s main.py /usr/lib/an-website/main.py
+systemctl enable an-website.service
+
+[ ! -f /usr/lib/an-website/main.py ] && mkdir /usr/lib/an-website && ln -s main.py /usr/lib/an-website/main.py
 
 systemctl restart an-website.service
