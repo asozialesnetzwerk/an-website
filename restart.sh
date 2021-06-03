5770e122e26caf18f8b54d34396f72eceea7a010
Joshix <joshix.dev@mailbox.org>
2021-06-03 23:42:34 +0200

    fix link

--- restart.sh
+++ restart.sh
@@ -7,6 +7,6 @@ cp an-website.service /etc/systemd/system/an-website.service
 systemctl enable an-website.service
 
 [ ! -f /usr/lib/an-website/main.py ] && mkdir /usr/lib/an-website
-[ ! -f /usr/lib/an-website/main.py ] && ln -s main.py /usr/lib/an-website/main.py
+[ ! -f /usr/lib/an-website/main.py ] && ln main.py /usr/lib/an-website/main.py
 
 systemctl restart an-website.service
