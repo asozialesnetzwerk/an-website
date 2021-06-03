372d72d4a7543ece32f4f3366cc6aebde27a6b51
Joshix <joshix.dev@mailbox.org>
2021-06-03 23:53:14 +0200

    fix copying

--- restart.sh
+++ restart.sh
@@ -7,7 +7,7 @@ cp an-website.service /etc/systemd/system/an-website.service
 
 [ ! -d /usr/lib/an-website/ ] && mkdir /usr/lib/an-website
 
-cp * /usr/lib/an-website/
+cp -r * /usr/lib/an-website/
 
 systemctl enable an-website.service
 systemctl restart an-website.service
