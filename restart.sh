72043d688d1a376485ec8e6b625a432f644e96d2
Joshix <joshix.dev@mailbox.org>
2021-06-04 00:22:09 +0200

    fix stupid error

--- restart.sh
+++ restart.sh
@@ -6,16 +6,16 @@ SERVICE_FILE_LOCATION="/etc/systemd/system/$SERVICE_FILE_NAME"
 # get latest files from git:
 git pull
 
-if [ ! -f "SERVICE_FILE_LOCATION" ]
+if [ ! -f "$SERVICE_FILE_LOCATION" ]
 then
     # File doesn't exist, so create it:
-    sudo touch "SERVICE_FILE_LOCATION"
+    sudo touch "$SERVICE_FILE_LOCATION"
     # Let the user be the owner:
-    sudo chown "$USER" "SERVICE_FILE_LOCATION"
+    sudo chown "$USER" "$SERVICE_FILE_LOCATION"
 fi
 
 # update the service file
-sed "s/user-placeholder/$USER/g" "$SERVICE_FILE_NAME" > "SERVICE_FILE_LOCATION"
+sed "s/user-placeholder/$USER/g" "$SERVICE_FILE_NAME" > "$SERVICE_FILE_LOCATION"
 
 # enable the service
 systemctl enable "$SERVICE_FILE_NAME"
