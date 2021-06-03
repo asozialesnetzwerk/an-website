107cd044383f6f7195a728478bdf936a98aa2d9d
Joshix <joshix.dev@mailbox.org>
2021-06-04 00:18:23 +0200

    improve restart.sh

--- restart.sh
+++ restart.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+SERVICE_FILE_NAME="an-website.service"
+SERVICE_FILE_LOCATION="/etc/systemd/system/$SERVICE_FILE_NAME"
+
+# get latest files from git:
 git pull
 
-sed "s/user-placeholder/$USER/g" an-website.service > /etc/systemd/system/an-website.service
+if [ ! -f "SERVICE_FILE_LOCATION" ]
+then
+    # File doesn't exist, so create it:
+    sudo touch "SERVICE_FILE_LOCATION"
+    # Let the user be the owner:
+    sudo chown "$USER" "SERVICE_FILE_LOCATION"
+fi
 
-systemctl enable an-website.service
-systemctl restart an-website.service
+# update the service file
+sed "s/user-placeholder/$USER/g" "$SERVICE_FILE_NAME" > "SERVICE_FILE_LOCATION"
+
+# enable the service
+systemctl enable "$SERVICE_FILE_NAME"
+# restart the service
+systemctl restart "$SERVICE_FILE_NAME"
