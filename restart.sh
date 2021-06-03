04fa252b3e2e599986fe4d729dfb42897d6a7eac
Joshix <joshix.dev@mailbox.org>
2021-06-04 01:58:20 +0200

    systemd → supervisor

--- restart.sh
+++ restart.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-SERVICE_FILE_NAME="an-website.service"
-SERVICE_FILE_LOCATION="$HOME/.config/systemd/user/"
+SERVICE_FILE_NAME="an-website.ini"
+SERVICE_FILE_LOCATION="/etc/supervisor.d/"
 
 # get latest files from git:
 git pull
 
 mkdir -p "$SERVICE_FILE_LOCATION"
 
-# update the service file
+# update the file
 sed "s/user-placeholder/$USER/g" "$SERVICE_FILE_NAME" > "$SERVICE_FILE_LOCATION$SERVICE_FILE_NAME"
 
-# enable the service
-systemctl --user enable "$SERVICE_FILE_NAME"
-# restart the service
-systemctl --user restart "$SERVICE_FILE_NAME"
+supervisorctl reread
+supervisorctl restart an-website
