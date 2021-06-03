a1ce6557bf32f3ce39a4afea9b25bf7ad94b9d47
Joshix <joshix.dev@mailbox.org>
2021-06-04 00:09:08 +0200

    make scripts better

--- restart.sh
+++ restart.sh
@@ -2,12 +2,7 @@
 
 git pull
 
-cp an-website.service /etc/systemd/system/an-website.service
-
-
-[ ! -d /usr/lib/an-website/ ] && mkdir /usr/lib/an-website
-
-cp -r * /usr/lib/an-website/
+sed "s/user-placeholder/$USER/g" an-website.service > /etc/systemd/system/an-website.service
 
 systemctl enable an-website.service
 systemctl restart an-website.service
