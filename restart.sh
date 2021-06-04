3f0a1eb72b346f7e050f7450c466a4d5ac782685
Joshix <joshix.dev@mailbox.org>
2021-06-04 02:01:28 +0200

    fix supervisor file

--- restart.sh
+++ restart.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
 SERVICE_FILE_NAME="an-website.ini"
-SERVICE_FILE_LOCATION="/etc/supervisor.d/"
+SERVICE_FILE_LOCATION="/etc/supervisor.d/$SERVICE_FILE_NAME"
 
 # get latest files from git:
 git pull
 
-mkdir -p "$SERVICE_FILE_LOCATION"
+
+if [ ! -f "$SERVICE_FILE_LOCATION" ]
+then
+    # File doesn't exist, so create it:
+    sudo touch "$SERVICE_FILE_LOCATION"
+    # Let the user be the owner:
+    sudo chown "$USER" "$SERVICE_FILE_LOCATION"
+fi
 
 # update the file
-sed "s/user-placeholder/$USER/g" "$SERVICE_FILE_NAME" > "$SERVICE_FILE_LOCATION$SERVICE_FILE_NAME"
+sed "s/user-placeholder/$USER/g" "$SERVICE_FILE_NAME" > "$SERVICE_FILE_LOCATION"
 
-supervisorctl reread
-supervisorctl restart an-website
+sudo supervisorctl reread
+sudo supervisorctl restart an-website
