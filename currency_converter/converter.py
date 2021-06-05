c860d06c1426c5bbddffae296bd774543755768e
Joshix <joshix.dev@mailbox.org>
2021-06-05 15:31:58 +0200

    add basics for currency converter

---
+++ currency_converter/converter.py
@@ -0,0 +1,55 @@
+import web
+
+keys = ["euro", "mark", "ost", "schwarz"]
+multipliers = [1, 2, 4, 20]
+
+
+def string_to_num(string, divide_by=1):
+    if string is None:
+        return None
+    return float(string.replace(",", ".")) / divide_by
+
+
+def num_to_string(num):
+    return f"{num:.2f}".replace(".", ",").replace(",00", "")
+
+
+def conversion_string(euro):
+    return f"{num_to_string(euro)} Euro, " \
+           f"das sind ja {num_to_string(euro * 2)} Mark; " \
+           f"{num_to_string(euro * 4)} Ostmark " \
+           f"und {num_to_string(euro * 20)} Ostmark auf dem Schwarzmarkt! "
+
+
+def get_value_dict(euro):
+    value_dict = {}
+
+    for i in range(len(keys)):
+        value_dict[keys[i]] = multipliers[i] * euro
+
+    value_dict["text"] = conversion_string(euro)
+
+    return value_dict
+
+
+def arguments_to_value_dict(web_input):
+    for i in range(len(keys)):
+        num_str = web_input.get(keys[i], None)
+        euro = string_to_num(num_str, multipliers[i])
+        if euro is not None:
+            return get_value_dict(euro)
+
+
+class CurrencyConverter:
+    def GET(self, test):
+        data = web.input()
+        euro = string_to_num(data.get("euro", "1"))
+        return str(get_value_dict(euro))
+
+
+class CurrencyConverterApi:
+    def GET(self, test):
+        value_dict = arguments_to_value_dict(web.input())
+        if value_dict is None:
+            return "Arguments: " + str(keys)
+        return str(value_dict)
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import web
 from discord.discord import Discord
+from currency_converter.converter import CurrencyConverter, CurrencyConverterApi
 
 urls = (
-    '/discord', 'Discord'
+    "/discord/?", "Discord",
+    "/(waehrungs-)?rechner/?", "CurrencyConverter",
+    "/(waehrungs-)?rechner/api/?", "CurrencyConverterApi"
 )
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
+
