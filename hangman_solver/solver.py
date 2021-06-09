f81a91790b65e51f0ef81e0ac1f36ddd07d65a7d
Joshix <joshix.dev@mailbox.org>
2021-06-09 22:53:07 +0200

    add get_word_dict function and make checkboxes work

--- hangman_solver/solver.py
+++ hangman_solver/solver.py
@@ -5,15 +5,32 @@ import json
 from utils.utils import get_url, RequestHandlerCustomError
 
 
+def get_word_dict(input_str="", words=[], letters={}, allow_umlauts=False, crossword_mode=False):
+    return {"input": input_str,
+            "words": words,
+            "letters": letters,
+            "allow_umlauts": allow_umlauts,
+            "crossword_mode": crossword_mode
+            }
+
+
 def find_words(request_handler):
-    input_str = request_handler.get_query_argument("input", default="")
-    invalid = request_handler.get_query_argument("invalid", default="")
-    allow_umlauts_str = request_handler.get_query_argument("has-umlauts", default="False")
+    allow_umlauts_str = request_handler.get_query_argument("allow-umlauts", default="False")
     crossword_mode_str = request_handler.get_query_argument("crossword-mode", default="False")
+    allow_umlauts = bool(strtobool(allow_umlauts_str))  # if the words can contain ä,ö,ü
+    crossword_mode = bool(strtobool(crossword_mode_str))  # if crossword mode
+
+    print("au", allow_umlauts )
+    print("cm", crossword_mode)
+
+    input_str = request_handler.get_query_argument("input", default="")
+    input_len = len(input_str)
+    if input_len == 0:  # input is empty:
+        return get_word_dict(allow_umlauts=allow_umlauts, crossword_mode=crossword_mode)
+
+    invalid = request_handler.get_query_argument("invalid", default="")
     language = request_handler.get_query_argument("lang", default="de")
 
-    allow_umlauts = bool(strtobool(allow_umlauts_str))  # if the words can contain ä,ö,ü
-    crossword_mode = bool(strtobool(crossword_mode_str))
     # in crossword_mode it doesn't matter if the letters are already in input_str:
     if not crossword_mode:
         invalid += input_str
@@ -25,7 +42,7 @@ def find_words(request_handler):
     if not os.path.isdir(folder):
         return {"error": "Invalid language."}
 
-    return {"input": input_str, "words": [input_str], "letters": {"p": 1, "e": 1, "s": 1, "n": 1, "i": 1}}
+    return get_word_dict(input_str, [input_str], {"p": 1, "e": 1, "s": 1, "n": 1, "i": 1}, allow_umlauts, crossword_mode)
 
 
 class HangmanSolver(RequestHandlerCustomError):
