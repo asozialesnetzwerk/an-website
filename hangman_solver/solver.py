63cc443651ba572bceb390a2555b45c0fda022c5
Joshix <joshix.dev@mailbox.org>
2021-06-14 21:31:26 +0200

    hangman_solver use dataclass

--- hangman_solver/solver.py
+++ hangman_solver/solver.py
@@ -1,9 +1,10 @@
 import os
 import re
+from dataclasses import dataclass, field, asdict
 from distutils.util import strtobool
-from typing import Dict, Iterable, Tuple, List
+from typing import Dict, Tuple, List
 
-from tornado.web import RequestHandler
+from tornado.web import RequestHandler, HTTPError
 
 from utils.utils import RequestHandlerCustomError, RequestHandlerJsonAPI, length_of_match
 
@@ -11,19 +12,16 @@ WILDCARDS_REGEX = re.compile(r"[_?-]+")
 NOT_WORD_CHAR = re.compile(r"[^a-zA-ZäöüßÄÖÜẞ]+")
 
 
-async def get_word_dict(input_str: str = "", invalid: str = "", words: list = None,
-                        crossword_mode: bool = False, max_words: int = 100, lang: str = "de_only_a-z") -> dict:
-    if words is None:
-        words = []
-    return {"input": input_str,
-            "invalid": invalid,
-            "lang": lang,
-            "crossword_mode": crossword_mode,
-            "max_words": max_words,
-            "words": words[:max_words],
-            "word_count": len(words),
-            "letters": await get_letters(words, input_str)
-            }
+@dataclass()
+class Hangman:
+    input: str = ""
+    invalid: str = ""
+    words: list[str] = field(default_factory=list[str])
+    word_count: int = 0
+    letters: dict[str, int] = field(default_factory=dict[str, int])
+    crossword_mode: bool = False
+    max_words: int = 100
+    lang: str = "de_only_a-z"
 
 
 async def generate_pattern_str(input_str: str, invalid: str, crossword_mode: bool) -> str:
@@ -58,60 +56,56 @@ async def search_words(file_name: str, pattern: str) -> list:
     return words
 
 
-async def get_letters(words: list, input_str: str) -> Dict[str, int]:
+async def get_letters(words: list, input_str: str) -> dict[str, int]:
     input_set = set(input_str.lower())
 
-    letters: Dict[str, int] = {}
+    letters: dict[str, int] = {}
     for word in words:
         for letter in set(word):
             if letter not in input_set:
-                letters[letter] = letters.get(letter, default=0) + 1
+                letters[letter] = letters.setdefault(letter, 0) + 1
 
     letters_items: List[Tuple[str, int]] = [(k, v) for k, v in letters.items()]
     sorted_letters: List[Tuple[str, int]] = sorted(letters_items, key=lambda item: item[1], reverse=True)
     return dict(sorted_letters)
 
 
-async def find_words(request_handler: RequestHandler) -> dict:
-    max_words = int(request_handler.get_query_argument("max_words", default="100"))
-    crossword_mode_str = request_handler.get_query_argument("crossword_mode", default="False")
+async def solve_hangman(request_handler: RequestHandler) -> Hangman:
+    max_words = int(str(request_handler.get_query_argument("max_words", default="100")))
+    crossword_mode_str = str(request_handler.get_query_argument("crossword_mode", default="False"))
     crossword_mode = bool(strtobool(crossword_mode_str))  # if crossword mode
 
-    language = request_handler.get_query_argument("lang", default="de")
+    language = str(request_handler.get_query_argument("lang", default="de"))
 
     folder = f"hangman_solver/words/words_{language}"
 
     if not os.path.isdir(folder):
-        return {"error": "Invalid language."}
+        raise HTTPError(400, f"'{language}' is an invalid language")
 
-    input_str = request_handler.get_query_argument("input", default="")
+    input_str = str(request_handler.get_query_argument("input", default=""))
     input_len = len(input_str)
     if input_len == 0:  # input is empty:
-        return await get_word_dict(crossword_mode=crossword_mode, max_words=max_words, lang=language)
+        return Hangman(crossword_mode=crossword_mode, max_words=max_words, lang=language)
 
-    invalid = request_handler.get_query_argument("invalid", default="")
+    invalid = str(request_handler.get_query_argument("invalid", default=""))
 
     file_name = f"{folder}/{input_len}.txt"
 
     pattern = await generate_pattern_str(input_str, invalid, crossword_mode)
     words = await search_words(file_name, pattern)
+    letters = await get_letters(words, input_str)
 
-    return await get_word_dict(input_str, invalid, words, crossword_mode, max_words)
+    return Hangman(input_str, invalid, words, len(words), letters, crossword_mode, max_words)
 
 
 class HangmanSolver(RequestHandlerCustomError):
     async def get(self, *args):
-        words_dict = await find_words(self)
+        hangman = await solve_hangman(self)
 
-        if words_dict.get("error"):
-            self.write_error(400, exc_info=words_dict.get("error"))
-            return
-
-        await self.render("pages/hangman_solver.html", **words_dict)
+        await self.render("pages/hangman_solver.html", **asdict(hangman))
 
 
 class HangmanSolverAPI(RequestHandlerJsonAPI):
     async def get(self, *args):
-        words_dict = await find_words(self)
-
-        self.write(words_dict)
+        hangman = await solve_hangman(self)
+        self.write(asdict(hangman))
