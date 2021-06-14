a8fd9831d9aa2d8ef4951c58c171ba50394f77ab
Guerteltier <guerteltier@asozial.org>
2021-06-14 20:10:00 +0000

    fix version.py

--- version/version.py
+++ version/version.py
@@ -3,10 +3,10 @@ import hashlib
 
 from ..utils.utils import RequestHandlerCustomError, run_shell_command
 
-VERSION = run_shell_command("git log -n1 --format=format:'%H'")
-FILE_HASHES = run_shell_command("git ls-files | xargs sha1sum")
+VERSION = run_shell_command("cd an_website && git log -n1 --format=format:'%H'")
+FILE_HASHES = run_shell_command("cd an_website && git ls-files | xargs sha1sum")
 HASH_OF_FILE_HASHES = hashlib.sha1(FILE_HASHES.encode("utf-8")).hexdigest()
-GH_PAGES_COMMIT_HASH = run_shell_command("git log -n1 --format=format:'%H' origin/gh-pages")
+GH_PAGES_COMMIT_HASH = run_shell_command("cd an_website && git log -n1 --format=format:'%H' origin/gh-pages")
 
 
 class Version(RequestHandlerCustomError):
