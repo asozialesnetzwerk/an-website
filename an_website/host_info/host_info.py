cf39e01b38b0bb29d507cc5757e1fd3a7665e865
Joshix-1 <Joshix-1@users.noreply.github.com>
2021-06-22 15:57:25 +0000

    fix formatting

--- an_website/host_info/host_info.py
+++ an_website/host_info/host_info.py
@@ -1,8 +1,9 @@
 from __future__ import annotations, barry_as_FLUFL
 
+from ansi2html import Ansi2HTMLConverter
+
 from ..utils.utils import BaseRequestHandler, run_shell
 
-from ansi2html import Ansi2HTMLConverter
 
 def get_handlers():
     return ((r"/host-info/?", Screenfetch),)
@@ -13,7 +14,4 @@ class Screenfetch(BaseRequestHandler):
         screen_fetch_result = (await run_shell("screenfetch"))[1].decode("utf-8")
         conv = Ansi2HTMLConverter(inline=True)
         html = conv.convert(screen_fetch_result, full=False)
-        await self.render(
-            "pages/host_info.html",
-            screen_fetch=html
-        )
+        await self.render("pages/host_info.html", screen_fetch=html)
