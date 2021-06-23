124d7c6725b38a70f210f7073769bf888fc03dce
Das Gürteltier <6079421+Guerteltier@users.noreply.github.com>
2021-06-23 14:00:11 +0200

    Update host_info.py

--- an_website/host_info/host_info.py
+++ an_website/host_info/host_info.py
@@ -6,12 +6,12 @@ from ..utils.utils import BaseRequestHandler, run_shell
 
 
 def get_handlers():
-    return ((r"/host-info/?", Screenfetch),)
+    return ((r"/host-info/?", HostInfo),)
 
 
-class Screenfetch(BaseRequestHandler):
+class HostInfo(BaseRequestHandler):
     async def get(self):
-        screen_fetch_result = (await run_shell("screenfetch"))[1].decode("utf-8")
+        screenfetch_result = (await run_shell("screenfetch"))[1].decode("utf-8")
         conv = Ansi2HTMLConverter(inline=True)
-        html = conv.convert(screen_fetch_result, full=False)
-        await self.render("pages/host_info.html", screen_fetch=html)
+        html = conv.convert(screenfetch_result, full=False)
+        await self.render("pages/host_info.html", screenfetch=html)
