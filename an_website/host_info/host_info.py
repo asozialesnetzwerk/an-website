2ca40d42c06c51ac4e0d8019be4a2a4be50e4162
Das Gürteltier <6079421+Guerteltier@users.noreply.github.com>
2021-06-23 14:03:30 +0200

    Update host_info.py

--- an_website/host_info/host_info.py
+++ an_website/host_info/host_info.py
@@ -2,7 +2,7 @@ from __future__ import annotations, barry_as_FLUFL
 
 from ansi2html import Ansi2HTMLConverter  # type: ignore
 
-from ..utils.utils import BaseRequestHandler, run_shell
+from ..utils.utils import BaseRequestHandler, run_exec
 
 
 def get_handlers():
@@ -11,7 +11,7 @@ def get_handlers():
 
 class HostInfo(BaseRequestHandler):
     async def get(self):
-        screenfetch_result = (await run_shell("screenfetch"))[1].decode("utf-8")
+        screenfetch_result = (await run_exec("screenfetch"))[1].decode("utf-8")
         conv = Ansi2HTMLConverter(inline=True)
         html = conv.convert(screenfetch_result, full=False)
         await self.render("pages/host_info.html", screenfetch=html)
