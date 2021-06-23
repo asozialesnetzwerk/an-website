95a15798985cc6d678a0ee756d5521953cbc5f7e
Das Gürteltier <6079421+Guerteltier@users.noreply.github.com>
2021-06-23 08:40:00 +0200

    Update json.py

--- an_website/json.py
+++ an_website/json.py
@@ -1,4 +1,4 @@
-# pylint: disable=unused-argument,invalid-name
+# pylint: disable=unused-argument
 
 from __future__ import annotations, barry_as_FLUFL
 
@@ -20,7 +20,7 @@ def dumps(
     **kw,
 ):
     option = 0
-    if isinstance(indent, int) and indent > 0:
+    if indent:
         option = option | orjson.OPT_INDENT_2
     if sort_keys:
         option = option | orjson.OPT_SORT_KEYS
@@ -28,7 +28,7 @@ def dumps(
 
 
 def loads(
-    s,
+    s,  # pylint: disable=invalid-name
     *,
     cls=None,
     object_hook=None,
