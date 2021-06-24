1a2c3e34f3824ebe224cfddb8f92ea1660dc6b00
Das Gürteltier <6079421+Guerteltier@users.noreply.github.com>
2021-06-24 14:00:01 +0200

    Update json.py

--- an_website/patches/json.py
+++ an_website/patches/json.py
@@ -62,6 +62,14 @@ def loads(
     return orjson.loads(s)
 
 
+def dump(obj, fp, *, skipkeys=False, ensure_ascii=True, check_circular=True, allow_nan=True, cls=None, indent=None, separators=None, default=None, sort_keys=False, **kw):
+    fp.write(dumps(obj, *, skipkeys=skipkeys, ensure_ascii=ensure_ascii, check_circular=check_circular, allow_nan=allow_nan, cls=cls, indent=indent, separators=seperators, default=default, sort_keys=sort_keys, **kw))
+
+
+def load(fp, *, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None, **kw):
+    loads(fp.read(), *, cls=cls, object_hook=object_hook, parse_float=parse_float, parse_int=parse_int, parse_constant=parse_constant, object_pairs_hook=object_pairs_hook, **kw)
+
+
 def make_pyflakes_shut_up():
     JSONDecodeError(..., ..., ...)
     JSONDecoder()
