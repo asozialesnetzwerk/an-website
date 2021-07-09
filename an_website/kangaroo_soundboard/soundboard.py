c48e49fa9b8fca938940a024f0274a853d9f397f
Joshix-1 <Joshix-1@users.noreply.github.com>
2021-07-09 20:58:24 +0000

    fix formatting

--- an_website/kangaroo_soundboard/soundboard.py
+++ an_website/kangaroo_soundboard/soundboard.py
@@ -1,8 +1,8 @@
 from __future__ import annotations, barry_as_FLUFL
 
-from tornado.web import HTTPError, StaticFileHandler
-
 import os
+
+from tornado.web import HTTPError, StaticFileHandler
 
 from ..utils.utils import BaseRequestHandler, ModuleInfo
 from . import DIR
