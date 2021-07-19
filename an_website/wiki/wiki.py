614699739c3b8854d6f0b6e33025c9f6e0fb5574
Joshix <joshix.dev@mailbox.org>
2021-07-19 21:01:33 +0200

    wiki page add basic handler

--- an_website/wiki/wiki.py
+++ an_website/wiki/wiki.py
@@ -1,24 +1,22 @@
 from __future__ import annotations, barry_as_FLUFL
 
-from tornado.web import StaticFileHandler
-
-from ..utils.utils import ModuleInfo
-from . import DIR
-
-PATH = f"{DIR}/build/"  # place with all build html files
-OPTIONS = {"path": PATH, "default_filename": "index.html"}
+from ..utils.utils import BaseRequestHandler, ModuleInfo
 
 
 def get_module_info() -> ModuleInfo:
     return ModuleInfo(
         handlers=(
             (
-                r"/wiki/(.*)",
-                StaticFileHandler,
-                OPTIONS,
+                r"/wiki(/?.*)",
+                WikiHandler,
             ),
         ),
         name="Asoziales Wiki",
         description="Ein Wiki mit Sachen des Asozialen Netzwerkes.",
-        path="/wiki/",  # the / at the end is important
+        path="/wiki",
     )
+
+
+class WikiHandler(BaseRequestHandler):
+    async def get(self, path):
+        return self.finish(f"/wiki{path} isn't ready yet.")
