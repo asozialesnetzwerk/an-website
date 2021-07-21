14e67412812e8cd71321a581ccbe99eae9d41f9a
Joshix <joshix.dev@mailbox.org>
2021-07-22 00:21:25 +0200

    add uptime viewer

---
+++ an_website/uptime/uptime.py
@@ -0,0 +1,32 @@
+from __future__ import annotations, barry_as_FLUFL
+
+import re
+import time
+import datetime
+from ..utils.utils import BaseRequestHandler, ModuleInfo
+
+START_TIME = time.time()
+
+
+def get_module_info() -> ModuleInfo:
+    return ModuleInfo(
+        handlers=((r"/version/?", Version),),
+        name="Betriebszeit",
+        description="Die Dauer die die Webseite am Stück in Betrieb ist.",
+        path="/version",
+    )
+
+
+class Version(BaseRequestHandler):
+    async def get(self):
+        uptime = datetime.timedelta(
+            seconds=round(time.time() - START_TIME, 3)
+        )
+
+        uptime_str = str(uptime)
+        uptime_str = re.sub("0{3}$", "", uptime_str, count=1)
+
+        await self.render(
+            "pages/uptime.html",
+            uptime=uptime_str,
+        )
