0b4618421efb943d9485cbb60b5a3648e817efb8
Joshix <joshix.dev@mailbox.org>
2021-09-04 13:34:14 +0200

    discord page, add api and don't link to disboard

--- an_website/discord/discord.py
+++ an_website/discord/discord.py
@@ -14,47 +14,83 @@
 """A permanent redirect to an invite of the discord guild."""
 from __future__ import annotations
 
+import logging
+
 import orjson
+import tornado.web
 from tornado.httpclient import AsyncHTTPClient, HTTPError
 
-from ..utils.request_handler import BaseRequestHandler
+from ..utils.request_handler import APIRequestHandler, BaseRequestHandler
 from ..utils.utils import ModuleInfo
 
-WIDGET_URL = "https://discord.com/api/guilds/367648314184826880/widget.json"
+GUILD_ID = 367648314184826880
 
 
 def get_module_info() -> ModuleInfo:
     """Create and return the ModuleInfo for this module."""
     return ModuleInfo(
-        handlers=((r"/discord/?", Discord),),
+        handlers=(
+            (r"/discord/?", Discord),
+            (r"/discord/(\d+)/?", Discord),
+            (r"/discord/api/?", DiscordApi),
+            (r"/discord/api/(\d+)/?", DiscordApi),
+        ),
         name="Discord-Einladung",
         description="Eine permanente Einladung zu unserem Discord-Server",
         path="/discord",
     )
 
 
+async def get_invite(guild_id: int = GUILD_ID) -> tuple[str, str]:
+    """
+    Get the invite to a discord guild and return it with the source.
+
+    How to get the invite:
+        - from the widget (has to be enabled in guild settings)
+        - from disboard (the guild needs to set it up first)
+    If the invite couldn't be fetched a HTTPError is thrown.
+    """
+    http_client = AsyncHTTPClient()
+
+    try:  # try getting the invite from the widget
+        url = f"https://discord.com/api/guilds/{guild_id}/widget.json"
+        response = await http_client.fetch(url)
+        response_json = orjson.loads(response.body.decode("utf-8"))
+        return response_json["instant_invite"], url
+    except HTTPError:
+        logging.error(HTTPError)
+        try:  # try getting the invite from disboard
+            url = f"https://disboard.org/site/get-invite/{guild_id}"
+            response = await http_client.fetch(url)
+            return orjson.loads(response.body.decode("utf-8")), url
+        except HTTPError:
+            logging.error(HTTPError)
+
+    raise tornado.web.HTTPError(404, reason="Invite not found.")
+
+
 class Discord(BaseRequestHandler):
     """The request handler that gets the discord invite and redirects to it."""
 
-    async def get(self):
+    async def get(self, guild_id=GUILD_ID):
         """Get the discord invite and redirect to it."""
-        http_client = AsyncHTTPClient()
-
         referrer = self.fix_url(
             self.request.headers.get("Referer", default="/")
         )
-        try:
-            response = await http_client.fetch(WIDGET_URL)
-        except HTTPError:
-            print()
-            self.redirect(
-                self.fix_url(
-                    "https://disboard.org/server/join/367648314184826880",
-                    referrer,
-                )
-            )
-        else:
-            response_json = orjson.loads(response.body.decode("utf-8"))
-            self.redirect(
-                self.fix_url(response_json["instant_invite"], referrer)
-            )
+        self.redirect(
+            self.fix_url((await get_invite(guild_id))[0], referrer)
+        )
+
+
+class DiscordApi(APIRequestHandler):
+    """The api request handler that gets the discord invite and returns it."""
+
+    async def get(self, guild_id=GUILD_ID):
+        """Get the discord invite and render it as json."""
+        invite, source_url = await get_invite(guild_id)
+        await self.finish(
+            {
+                "invite": invite,
+                "source": source_url
+            }
+        )
