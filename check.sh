4a599ccfeb68a4eba372e9007e1ac0cb76fc9c84
Joshix <joshix.dev@mailbox.org>
2021-09-14 20:39:02 +0200

    update check.sh to check tests

--- check.sh
+++ check.sh
@@ -16,25 +16,25 @@ git ls-files | xargs sha1sum | sha1sum | cut -d ' ' -f 1
 
 # check for errors
 echo Pyflakes:
-python3 -m pyflakes an_website || exit 1
+python3 -m pyflakes an_website tests || exit 1
 
 # sort imports
 echo isort:
-python3 -m isort an_website
+python3 -m isort an_website tests
 
 # check formatting
 echo Black:
-python3 -m black --check --diff --color an_website || echo 'Run "python3 -m black an_website" to reformat.'
+python3 -m black --check --diff --color an_website tests || echo 'Run "python3 -m black an_website tests" to reformat.'
 
 # check types
 echo mypy:
-python3 -m mypy --pretty -p an_website
+python3 -m mypy --pretty -p an_website -p tests
 
 # lint
 echo Flake8:
-python3 -m flake8 --extend-ignore=D100,D101,D102,D103,D104,E501 an_website
+python3 -m flake8 --extend-ignore=D100,D101,D102,D103,D104,E501 an_website tests
 echo Pylint:
-python3 -m pylint --output-format=colorized an_website
+python3 -m pylint --output-format=colorized an_website tests
 
 # run tests
 echo Tests:
