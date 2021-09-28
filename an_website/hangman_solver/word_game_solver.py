0b0798d9a0d2823fb8f070f071862c36c7140eb8
Joshix <joshix.dev@mailbox.org>
2021-09-28 20:10:52 +0200

    fix code style

--- an_website/hangman_solver/word_game_solver.py
+++ an_website/hangman_solver/word_game_solver.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Module for the word game helper"""
+"""Module for the word game helper."""
 from __future__ import annotations
 
 from typing import Optional
