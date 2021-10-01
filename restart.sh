ad5b39cf4fd090c4c054adacbba689dfe47959c6
Joshix <joshix.dev@mailbox.org>
2021-10-01 17:27:08 +0200

    print comments in restart.sh

--- restart.sh
+++ restart.sh
@@ -6,24 +6,24 @@ SERVICE_FILE_LOCATION="/etc/supervisor.d/$SERVICE_FILE_NAME"
 # if it is in detached head state:
 if [ -z "$(git branch --show-current)" ]
 then
-    # go to main branch:
+    echo "checkout to main branch"
     git checkout main
 fi
 
-# get latest files from git:
+echo "get latest files from git"
 git pull --rebase --autostash
 
 # install required packages:
 if [ ! -d "venv" ]
 then
-    # venv doesn't exist, so create it:
+    echo "venv doesn't exist, so create it"
     python3 -m venv venv
 fi
 
 # if there's an argument use that commit hash
 if [ -n "$1" ]
 then
-    echo "$1"
+    echo "checkout to git hash: '$1'"
     git checkout "$1"
 fi
 
@@ -32,14 +32,18 @@ venv/bin/pip install --disable-pip-version-check -U -r requirements.txt
 
 if [ ! -f "$SERVICE_FILE_LOCATION" ]
 then
-    # File doesn't exist, so create it:
+    echo "supervisord config doesn't exist, so create it"
     sudo touch "$SERVICE_FILE_LOCATION"
-    # Let the user be the owner:
+    echo "Let the user be the owner"
     sudo chown "$USER" "$SERVICE_FILE_LOCATION"
 fi
 
-# update the file
+echo "Update the supervisord config in $SERVICE_FILE_LOCATION"
 sed "s/user-placeholder/$USER/g" "$SERVICE_FILE_NAME" > "$SERVICE_FILE_LOCATION"
 
+echo "Reread config"
 sudo supervisorctl reread
+echo "Restart an_website"
 sudo supervisorctl restart an_website
+
+echo "Finished restarting"
