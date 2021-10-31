ff424978ede86971366c873d770a8e51389ab3dc
Joshix <joshix.dev@mailbox.org>
2021-10-31 13:53:22 +0100

    use more js for quotes page to reduce request data

--- an_website/static/js/quote.js
+++ an_website/static/js/quote.js
@@ -17,11 +17,108 @@ document.addEventListener("keydown", function(event) {
     if(event.code === "ArrowUp" || event.code === "KeyW") {
         document.getElementById("upvote").click();
     } else if(event.code === "ArrowLeft" || event.code === "KeyA") {
-        window.history.go(-1);
+        window.history.back();
     } else if(event.code === "ArrowDown" || event.code === "KeyS") {
         document.getElementById("downvote").click();
     } else if(event.code === "ArrowRight" || event.code === "KeyD") {
         document.getElementById("next").click();
     }
 });
+
+const nextButton = document.getElementById("next");
+
+const shareButton = document.getElementById("share");
+const downloadButton = document.getElementById("download");
+const quoteIdDisplay = document.getElementById("quote-id");
+
+const author = document.getElementById("author");
+const quote = document.getElementById("quote");
+
+const upvoteButton = document.getElementById("upvote");
+const downvoteButton = document.getElementById("downvote");
+
+const ratingText = document.getElementById("rating-text");
+const ratingImg = document.getElementById("rating-image");
+
+const params = nextButton.href.includes("?")
+    ? "?" + nextButton.href.split("?", 2)[1]
+    : "";
+
+nextButton.removeAttribute("href");
+
+function updateQuoteId(quoteId) {
+    shareButton.href = `/zitate/${quoteId}/share/${params}`;
+    downloadButton.href = `/zitate/${quoteId}/image.png${params}`;
+    quoteIdDisplay.innerText = quoteId;
+    const [q_id, a_id] = quoteId.split("-", 2);
+    quote.href = `/zitate/info/q/${q_id}/${params}`;
+    author.href = `/zitate/info/a/${a_id}/${params}`;
+}
+
+function updateRating(rating) {
+    ratingText.innerText = rating;
+    if (rating === "---" || rating === 0) {
+        ratingImg.className = "invisible";
+        ratingImg.src = "";
+        ratingImg.alt = "";
+    } else if (Number.parseInt(rating) > 0) {
+        ratingImg.className = "witzig";
+        ratingImg.src = "/static/img/StempelWitzig.svg";
+        ratingImg.alt = "Witzig-Stempel";
+    } else if (Number.parseInt(rating)  < 0) {
+        ratingImg.className = "nicht-witzig";
+        ratingImg.src = "/static/img/StempelNichtWitzig.svg";
+        ratingImg.alt = "Nicht-Witzig-Stempel";
+    }
+}
+
+function updateVote(vote) {
+    if (vote === 1) {
+        upvoteButton.classList.add("voted");
+        upvoteButton.value = "0";
+    } else {
+        upvoteButton.classList.remove("voted");
+        upvoteButton.value = "1";
+    }
+    if (vote === -1) {
+        downvoteButton.classList.add("voted");
+        downvoteButton.value = "0";
+    } else {
+        downvoteButton.classList.remove("voted");
+        downvoteButton.value = "1";
+    }
+}
+
+function handleData(data) {
+    updateQuoteId(data["id"]);
+    nextQuoteId[0] = data["next"];
+    quote.innerText = `»${data["quote"]}«`;
+    author.innerText = `- ${data["author"]}`;
+    updateRating(data["rating"]);
+    updateVote(data["vote"]);
+}
+
+window.onpopstate = (event) => {
+    const data = event.state;
+    if (data) {
+        handleData(data);
+    }
+}
+
+nextButton.onclick = () => {
+    fetch(`/zitate/api/${nextQuoteId[0]}/`).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        if (data) {
+            window.history.pushState(
+                data,
+                "Falsche Zitate",
+                `/zitate/${data["id"]}/${params}`
+            );
+            handleData(data);
+        }
+    }).catch(function(error) {
+        console.error(error);
+    });
+}
 // @license-end
