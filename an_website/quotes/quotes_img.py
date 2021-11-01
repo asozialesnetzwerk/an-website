fdfb840278b9f24880c646329bc8837e1d599008
Joshix <joshix.dev@mailbox.org>
2021-11-01 12:58:03 +0100

    remove line only intended for debugging purpose

--- an_website/quotes/quotes_img.py
+++ an_website/quotes/quotes_img.py
@@ -194,7 +194,6 @@ class QuoteAsImg(QuoteReadyCheckRequestHandler):
     async def get(self, quote_id: str, author_id: str):
         """Handle the get request to this page and render the quote as img."""
         wrong_quote = await get_wrong_quote(int(quote_id), int(author_id))
-        self.request.host_name = "asozial.net"
 
         if not (
             str_to_bool(
