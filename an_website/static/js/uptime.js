14e1b8d3437665f0c472eefcdbf924433b40420f
Joshix-1 <joshix.dev@mailbox.org>
2021-11-02 10:23:19 +0100

    remove long license text from js files to reduce file size

--- an_website/static/js/uptime.js
+++ an_website/static/js/uptime.js
@@ -1,18 +1,4 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
-/*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
 const uptimeDiv = document.getElementById("uptime");
 // uptimeAtStart is set in the html document
 // the time the website got loaded minus the uptime
