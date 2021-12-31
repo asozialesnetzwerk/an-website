a27e06c59da119bfd9dff1caa52fe677d109ad60
Joshix <joshix.dev@mailbox.org>
2021-12-31 15:31:47 +0100

    improve command handling and add explaining dicts 🃏

--- an_website/nimm_mal_kurz/game.py
+++ an_website/nimm_mal_kurz/game.py
@@ -16,7 +16,9 @@ from __future__ import annotations
 
 import os
 import uuid
-from typing import Any
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, Literal
 
 import orjson as json
 import tornado.websocket as websocket
@@ -29,11 +31,73 @@ from tornado.web import HTTPError
 PH = PasswordHasher()  # nicht die Webseite
 
 
+class ValueType(Enum):
+    """The type of value."""
+
+    STRING = "String"
+
+
+@dataclass(frozen=True)
+class ArgumentInfo:
+    """Information about an argument."""
+
+    name: str
+    description: str
+    optional: bool
+    default: Any
+    value_type: ValueType = ValueType.STRING
+
+
+@dataclass(frozen=True)
+class CommandInfo:
+    """Information about a command."""
+
+    name: str
+    description: str
+    arguments: tuple[ArgumentInfo, ...]
+    response: tuple[str, ...]  # all the response keys except of type
+    response_types: tuple[str, ...]  # the values the "type" response can have
+
+
+COMMANDS_IN_ROOM: dict[str, CommandInfo] = {
+    "leave_room": CommandInfo(
+        "leave_room",
+        "Leave the current room.",
+        tuple(),
+        tuple(),
+        ("error", "left_room"),
+    ),
+}
+
+# commands that can only be used as the first command of the ws connection
+LOGIN_COMMANDS: dict[str, CommandInfo] = {
+    "init": CommandInfo(
+        "init",
+        "Initialize a new user.",
+        (ArgumentInfo("name", "The name of the user.", optional=True),),
+        ("user_id", "auth_key", "name"),
+        ("error", "login"),
+    ),
+    "login": CommandInfo(
+        "login",
+        "Login as an existing user.",
+        (
+            ArgumentInfo("user_id", "The id of the user.", optional=False),
+            ArgumentInfo(
+                "auth_key", "The auth key of the user.", optional=False
+            ),
+            ArgumentInfo("name", "The name of the user.", optional=True),
+        ),
+        ("userid", "name"),
+        ("error", "login"),
+    ),
+}
+
+
 class GameWebsocket(websocket.WebSocketHandler):
     """The Websocket handler."""
 
     user_id: None | str = None
-    room_id: None | str = None
 
     async def open(self, *args) -> None:
         print("WebSocket opened")
@@ -48,9 +112,10 @@ class GameWebsocket(websocket.WebSocketHandler):
                 {"type": "error", "message": "No command"}
             )
             return
+        command = command.lower()
         if command == "init":
             self.user_id = await self.login_as_new_user(
-                json_message.get("nickname")
+                json_message.get("name")
             )
         elif command == "login":
             self.user_id = await self.login(json_message)
@@ -58,7 +123,45 @@ class GameWebsocket(websocket.WebSocketHandler):
             self.close(reason="Please log in with the first message.")
             return
 
+        user_in_room = False
+        room_id = await self.redis.get(
+            self.get_redis_key("user", self.user_id, "room")
+        )
+        if room_id:
+            if await self.redis.sismember(
+                self.get_redis_key("room", room_id, "users"),
+                self.user_id,
+            ):
+                user_in_room = True
+            await self.handle_commands_in_room(room_id, command, json_message)
+
+        if user_in_room:
+            if command in COMMANDS_IN_ROOM:
+                await self.handle_commands_in_room(
+                    room_id, command, json_message
+                )
+            else:  # TODO: special case for when playing
+                await self.write_message(
+                    {"type": "error", "message": "Command not allowed in room"}
+                )
+            return
+
+        if command == "create_room":
+            await self.create_room()
+        elif command == "join_room":
+            await self.join_room()
+        elif command == "list_public_rooms":
+            await self.list_public_rooms()
+        return
+
         # TODO: Handle commands
+
+    async def handle_commands_in_room(
+        self, room_id: str, command: str, json_msg: dict[str, Any]
+    ) -> None:
+        """Handle commands that are only allowed in a room."""
+        if command == "leave_room":
+            await self.leave_room(room_id)
 
     async def on_close(self):
         print("WebSocket closed")
@@ -90,24 +193,23 @@ class GameWebsocket(websocket.WebSocketHandler):
 
         if user_id and PH.verify(user.get("auth_key_hash"), auth_key):
             # auth key verified
-            if nickname := json_message.get("nickname"):
-                if len(nickname) > 69:  # nice
-                    nickname = nickname[:69]
+            if name := json_message.get("name"):
+                if len(name) > 69:  # nice
+                    name = name[:69]
                 await self.elasticsearch.update(
                     index=self.elasticsearch_prefix + "-halt_mal_kurz-users",
                     id=user_id,
                     # TODO: read the fucking docs
-                    body={"doc": {"nickname": nickname}},
+                    body={"doc": {"name": name}},
                 )
             else:
-                nickname = user.get("nickname")
+                name = user.get("name")
 
             await self.write_message(
                 {
                     "type": "login",
                     "user_id": user_id,
-                    "auth_key": auth_key,
-                    "nickname": nickname,
+                    "name": name,
                 }
             )
             return user_id
@@ -135,22 +237,20 @@ class GameWebsocket(websocket.WebSocketHandler):
         """Get the Elasticsearch prefix from the settings."""
         return self.settings.get("ELASTICSEARCH_PREFIX", str())
 
-    async def login_as_new_user(
-        self, nickname: None | str = None
-    ) -> str | None:
+    async def login_as_new_user(self, name: None | str = None) -> str | None:
         """Login as a new user."""
         user_id = uuid.uuid4().hex
         auth_key = os.urandom(32).hex()
-        nickname = nickname or generate_name(style="capital")
-        if len(nickname) > 69:  # nice
-            nickname = nickname[:69]
+        name = name or generate_name(style="capital")
+        if len(name) > 69:  # nice
+            name = name[:69]
         await self.elasticsearch.index(
             index=self.elasticsearch_prefix + "-halt_mal_kurz-users",
             id=user_id,
             document={
                 "user_id": user_id,
                 "auth_key_hash": PH.hash(auth_key),
-                "nickname": nickname,
+                "name": name,
             },
         )
         await self.write_message(
@@ -158,7 +258,7 @@ class GameWebsocket(websocket.WebSocketHandler):
                 "type": "login",
                 "user_id": user_id,
                 "auth_key": auth_key,
-                "nickname": nickname,
+                "name": name,
             }
         )
         return user_id
@@ -184,20 +284,96 @@ class GameWebsocket(websocket.WebSocketHandler):
             return f"{prefix}:nimm-mal-kurz"
         return f"{prefix}:nimm-mal-kurz:{':'.join(args)}"
 
-    async def is_in_room(self) -> None | str:
-        """Check if the user is in a room the room ID."""
-        user_id = self.user_id
-        if not user_id:
-            return None
-        # get the room if from the user id with redis
-        room_id = await self.redis.get(
-            self.get_redis_key("user", user_id, "room")
+    async def create_room(self, json_msg: dict[str, Any]):
+        """Create a new room."""
+        # Create room with settings from the request
+        room_id = str(uuid.uuid4())
+        settings = {
+            "name": self.get_argument("name", str()),
+            "password": self.get_argument("password", str()),
+            "max_players": self.get_argument("max_players", "5"),
+            "cards": self.get_argument("cards", "K=10-S|3≤S≤5"),
+            "max_time": self.get_argument("max_time", "-1"),
+            "max_time_per_card": self.get_argument(
+                "max_time_per_card", "30"  # in seconds
+            ),
+            "owner": self.get_user_id(),
+        }
+        await self.redis.hset(
+            self.get_redis_key("room", room_id, "settings"), mapping=settings
         )
-        if not room_id:
-            return None
-        if await self.redis.sismember(
+
+    async def join_room(self):
+        """Join a room."""
+        if not self.get_room_id_specified_by_user():
+            raise HTTPError(400, reason="No room ID provided.")
+        # TODO: Check if user is already in a room
+        # if None not in (self.get_room_id()):
+        #     raise HTTPError(400, reason="User is currently in a room.")
+        room_id = self.room_id or self.redis.get("user", self.user_id, "room")
+
+        player_count = len(
+            await self.redis.smembers(
+                self.get_redis_key("room", room_id, "users")
+            )
+        )
+        if player_count >= int(
+            await self.redis.hget(
+                self.get_redis_key("room", room_id, "settings"), "max_players"
+            )
+        ):
+            raise HTTPError(400, reason="Room is full.")
+
+        if _pw := self.redis.hget(  # pw check
+            self.get_redis_key("room", room_id, "settings"), "password"
+        ):
+            if _pw != self.get_argument("password", str()):
+                raise HTTPError(400, reason="Wrong password.")
+
+        if not self.redis.sadd(
             self.get_redis_key("room", room_id, "users"),
             user_id,
         ):
-            return room_id
-        return None
+            raise HTTPError(500, reason="Could not add user to room.")
+        await self.redis.set(
+            self.get_redis_key("user", user_id, "room"),
+            room_id,
+        )
+        if not player_count:
+            await self.redis.hset(
+                self.get_redis_key("room", room_id, "settings"),
+                "owner",
+                user_id,
+            )
+
+        await self.write_message(
+            {
+                "type": "joined_room",
+                "room": {
+                    "id": room_id,
+                    "settings": await self.get_room_settings(room_id),
+                },
+            }
+        )
+
+    async def leave_room(self, room_id: str):
+        """Leave the room."""
+        users_redis_key = self.get_redis_key("room", room_id, "users")
+        if not await self.redis.srem(
+            users_redis_key,
+            self.user_id,
+        ):
+            raise HTTPError(500, reason="Could not remove user from room.")
+        await self.redis.delete(
+            self.get_redis_key("user", self.user_id, "room")
+        )
+        # check if user is the last one in the room
+        if not self.redis.smembers(users_redis_key):
+            # TODO: improve room deletion
+            await self.redis.delete(users_redis_key)
+            await self.redis.delete(
+                self.get_redis_key("room", room_id, "settings")
+            )
+
+        # TODO: Do stuff when game is running
+        await self.write_message({"type": "left_room"})
