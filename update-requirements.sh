b4d2b8b85ea80a8de926a9316819966f59bf27a9
Das Gürteltier <guerteltier@asozial.org>
2022-03-11 21:00:00 +0000

    fix stuff ✨

--- update-requirements.sh
+++ update-requirements.sh
@@ -2,3 +2,6 @@
 
 PIPENV_VERBOSITY=-1 pipenv lock -r > requirements.txt
 PIPENV_VERBOSITY=-1 pipenv lock -r --dev > requirements-dev.txt
+
+sed -i "s/;.*//" requirements.txt
+sed -i "s/;.*//" requirements-dev.txt
