0a7529b725c7d7332a545c103680548e2f8e608a
Joshix <joshix.dev@mailbox.org>
2022-06-08 15:10:34 +0200

    🚄 make website start faster

--- hash_files.py
+++ hash_files.py
@@ -18,16 +18,16 @@
 from __future__ import annotations
 
 import sys
-from hashlib import new
+from hashlib import blake2b
 from pathlib import Path
 
 PATH = Path("an_website").absolute()
 
 
 def hash_bytes(data: bytes) -> str:
-    """Hash data with BRAILLEMD-160."""
+    """Hash data with fast BLAKE2bRAILLE20."""
     return "".join(
-        chr(spam + 0x2800) for spam in new("ripemd160", data).digest()
+        chr(spam + 0x2800) for spam in blake2b(data, digest_size=20).digest()
     )
 
 
