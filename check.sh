52f5fa8c5aa5a9c39cad408c051635a1ec7581ce
Das Gürteltier <guerteltier@asozial.org>
2022-08-07 20:00:00 +0000

    ✨ improve stuff

--- check.sh
+++ check.sh
@@ -9,15 +9,15 @@ if [ -d venv ]; then
 fi
 
 pip_install="python3 -m pip install --disable-pip-version-check --require-virtualenv --quiet"
-$pip_install "pip>=22.2" wheel
+${pip_install} "pip>=22.2" wheel
 exit_code="$?"
-if [ "$exit_code" -ne 0 ] && [ "$exit_code" -ne 3 ]; then
+if [ "${exit_code}" -ne 0 ] && [ "${exit_code}" -ne 3 ]; then
   echo "Installing pip>=22.2 and wheel failed."
   exit 1
 fi
-$pip_install -r requirements-dev.txt
+${pip_install} -r requirements-dev.txt
 exit_code="$?"
-if [ "$exit_code" -ne 0 ] && [ "$exit_code" -ne 3 ]; then
+if [ "${exit_code}" -ne 0 ] && [ "${exit_code}" -ne 3 ]; then
   echo "Installing requirements in requirements-dev.txt failed."
   exit 1
 fi
@@ -52,13 +52,13 @@ if [ -n "$1" ]; then
   pytest="python3 -m pytest --durations=0 --durations-min=0.5"
   if [ "$1" = "test" ]; then
     echo Tests:
-    $pytest tests || FAILED=$(( 128 | FAILED ))
+    ${pytest} tests || FAILED=$(( 128 | FAILED ))
   elif [ "$1" = "test-cov" ]; then
     echo Tests:
-    $pytest --cov=an_website --cov-report= tests || FAILED=$(( 128 | FAILED ))
+    ${pytest} --cov=an_website --cov-report= tests || FAILED=$(( 128 | FAILED ))
     echo Coverage:
     python3 -m coverage report --precision=3 --sort=cover --skip-covered
   fi
 fi
 
-exit "$FAILED"
+exit "${FAILED}"
