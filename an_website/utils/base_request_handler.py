43928a7fb5d47849bc3bf1967220a5503beb35da
Joshix-1 <joshix.dev@mailbox.org>
2023-05-01 17:00:00 +0000

    🧪 stop tests from failing v2

--- an_website/utils/base_request_handler.py
+++ an_website/utils/base_request_handler.py
@@ -105,10 +105,6 @@ TEXT_CONTENT_TYPES: Final[set[str]] = {
     "application/yaml",
 }
 
-FIXED_ANSI_REGEX: Final = __import__("re").compile(
-    "\033\\[([-\\d;:]*)([a-zA-z])"
-)
-
 
 class BaseRequestHandler(RequestHandler):
     """The base request handler used by every page and API."""
@@ -629,10 +625,10 @@ class BaseRequestHandler(RequestHandler):
         description and no_3rd_party).
         """
         namespace = super().get_template_namespace()
-        converter = Ansi2HTMLConverter(inline=True, scheme="xterm")
-        converter.ansi_codes_prog = FIXED_ANSI_REGEX
+        ansi2html = Ansi2HTMLConverter(inline=True, scheme="xterm").convert
+        ansi_b_replace = partial(regex.sub, "\033\\[-?\\dB", "")
         namespace.update(
-            ansi2html=partial(converter.convert, full=False),
+            ansi2html=lambda _: ansi_b_replace(ansi2html(_, full=False)),
             as_html=self.content_type == "text/html",
             bumpscosity=self.get_bumpscosity(),
             c=self.now.date() == date(self.now.year, 4, 1)
