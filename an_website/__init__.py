e740982567b534a8393ff9960461245fe8f83924
Not Committed Yet <guerteltier@asozial.org>
2023-12-01 20:00:00 +0000

    🎅 fix workflow

--- an_website/__init__.py
+++ an_website/__init__.py
@@ -32,7 +32,7 @@ if sys.version_info < (3, 12):
         import orjson as json
 
 if "orjson" not in sys.modules:
-    from . import orjson_temp_fix as json  # noqa: F811
+    from . import orjson_temp_fix as json  # type: ignore[no-redef]  # noqa: F811
 
     sys.modules["orjson"] = json
 
