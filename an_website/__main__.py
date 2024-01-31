305153cd50bf7b7db64fcec3a44e57c66f440056
Not Committed Yet <guerteltier@asozial.org>
2024-01-31 18:00:00 +0000

    ✨ update stuff

--- an_website/__main__.py
+++ an_website/__main__.py
@@ -22,7 +22,7 @@ import sys
 import tracemalloc
 import warnings
 
-from dill._dill import PickleWarning  # type: ignore[import-untyped]
+from dill._dill import PickleWarning  # type: ignore[import-untyped]  # nosec: B403
 
 if sys.flags.dev_mode and not (
     "PYTHONWARNINGS" in os.environ or " -W" in " ".join(sys.orig_argv)
