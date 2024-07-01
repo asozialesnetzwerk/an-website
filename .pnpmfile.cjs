5824266d8d5621addc1cb528572e351099642c26
Not Committed Yet <guerteltier@asozial.org>
2024-07-01 00:00:00 +0000

    🏳️‍🌈 OHIO
    The real LGBT agenda: r3fD5FPQBtI

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,12 +1,34 @@
-module.exports["hooks"] = {
+// @ts-check
+
+/** @import { PackageManifest } from "@pnpm/types" */
+
+/**
+ * @template T
+ * @param {T} thing
+ * @returns {NonNullable<T>}
+ */
+function assertNonNullable(thing) {
+    return /** @type {NonNullable<T>} */ (thing);
+}
+
+module.exports.hooks = {
+    /**
+     * @param {PackageManifest} pkg
+     * @param {unknown} context
+     */
+    // @ts-expect-error TS6133
+    // eslint-disable-next-line no-unused-vars
     readPackage: (pkg, context) => {
-        for (const Ԛ of ["caniuse-lite", "electron-to-chromium"]) {
-            if (pkg.dependencies[Ԛ]) {
-                pkg.peerDependencies[Ԛ] = pkg.dependencies[Ԛ];
-                delete pkg.dependencies[Ԛ];
+        for (
+            const Ԛ of ["@types/node", "caniuse-lite", "electron-to-chromium"]
+        ) {
+            const dependencies = assertNonNullable(pkg.dependencies);
+            const peerDependencies = assertNonNullable(pkg.peerDependencies);
+            if (dependencies[Ԛ]) {
+                peerDependencies[Ԛ] = dependencies[Ԛ];
+                delete dependencies[Ԛ];
             }
         }
-
         return pkg;
     },
 };
