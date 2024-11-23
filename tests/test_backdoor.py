aa82086078c1cd6c90b6951f18f44428813a8a67
Joshix <joshix.dev@mailbox.org>
2024-11-23 12:00:00 +0000

    🌪 update dependencies

--- tests/test_backdoor.py
+++ tests/test_backdoor.py
@@ -15,10 +15,14 @@
 
 from __future__ import annotations
 
+import pickle  # nosec: B403
+from collections.abc import Callable
+from functools import partial
 from types import EllipsisType
 from typing import Any, Literal, TypedDict, cast
 
 import dill  # type: ignore[import-untyped]  # nosec: B403
+import jsonpickle  # type: ignore[import-untyped]
 
 from . import (  # noqa: F401  # pylint: disable=unused-import
     FetchCallable,
@@ -40,12 +44,15 @@ class Response(TypedDict):  # noqa: D101
 ErrorTuple = tuple[int, str]
 
 
-async def request_and_parse(
+async def _request_and_parse(
     # pylint: disable=redefined-outer-name, too-many-arguments
     fetch: FetchCallable,  # noqa: F811
     /,
     code: str,
     *,
+    accept: str,
+    loads: Callable[[bytes], Any],
+    content_type: str | None = None,
     session: None | str = None,
     auth_key: None | str = None,
     mode: Literal["exec", "eval"] = "eval",
@@ -53,7 +60,7 @@ async def request_and_parse(
 ) -> Response | ErrorTuple | str | None:
     """Make a request to the backdoor and parse the response."""
     auth_key = "123qweQWE!@#000000000" if auth_key is None else auth_key
-    headers = {"Authorization": auth_key}
+    headers = {"Authorization": auth_key, "Accept": accept}
 
     if session:
         headers["X-Backdoor-Session"] = session
@@ -68,11 +75,9 @@ async def request_and_parse(
         body=code,
     )
 
-    assert_valid_response(http_response, "application/vnd.uqfoundation.dill")
+    assert_valid_response(http_response, content_type or accept)
 
-    response: Response | ErrorTuple | str | None = dill.loads(  # nosec: B301
-        http_response.body
-    )
+    response: Response | ErrorTuple | str | None = loads(http_response.body)
 
     assert isinstance(response, dict | tuple | str | None)
 
@@ -93,146 +98,174 @@ async def request_and_parse(
         assert isinstance(response["result"][1], bytes | None)
         if isinstance(response["result"][1], bytes):
             result = list(response["result"])
-            result[1] = dill.loads(result[1])  # nosec: B301
+            result[1] = loads(result[1])
             response["result"] = cast(Result, tuple(result))
 
     return response
 
 
+request_and_parse = partial(
+    _request_and_parse,
+    accept="*/*",
+    content_type="application/vnd.uqfoundation.dill",
+    loads=dill.loads,
+)
+
+
 async def test_backdoor(fetch: FetchCallable) -> None:  # noqa: F811
     """Test the backdoor."""
     # pylint: disable=too-many-statements
-    response = await request_and_parse(fetch, "0==0")
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert not response["output"]
-    assert response["result"] == ("True", True)
+    for request in (
+        partial(
+            _request_and_parse,
+            accept="*/*",
+            content_type="application/vnd.uqfoundation.dill",
+            loads=pickle.loads,  # nosec: B301
+        ),
+        partial(
+            _request_and_parse,
+            accept="application/vnd.uqfoundation.dill",
+            loads=dill.loads,  # nosec: B301
+        ),
+        partial(
+            _request_and_parse,
+            accept="application/json",
+            loads=partial(
+                jsonpickle.loads, on_missing="error", classes=type(...)
+            ),
+        ),
+    ):
+        response = await request(fetch, "0==0")
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert not response["output"]
+        assert response["result"] == ("True", True)
 
-    response = await request_and_parse(
-        fetch, "(0<>0)", features="barry_as_FLUFL"
-    )
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert not response["output"]
-    assert response["result"] == ("False", False)
+        response = await request(fetch, "(0<>0)", features="barry_as_FLUFL")
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert not response["output"]
+        assert response["result"] == ("False", False)
 
-    response = await request_and_parse(fetch, "print('spam');", mode="eval")
-    assert isinstance(response, dict)
-    assert not response["success"]
-    assert not response["output"]
-    assert isinstance(response["result"], tuple)
-    assert isinstance(response["result"][1], SyntaxError)
+        response = await request(fetch, "print('spam');", mode="eval")
+        assert isinstance(response, dict)
+        assert not response["success"]
+        assert not response["output"]
+        assert isinstance(response["result"], tuple)
+        assert isinstance(response["result"][1], SyntaxError)
 
-    response = await request_and_parse(fetch, "print('spam');", mode="exec")
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert response["output"] == "spam\n"
-    assert not response["result"]
+        response = await request(fetch, "print('spam');", mode="exec")
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert response["output"] == "spam\n"
+        assert not response["result"]
 
-    response = await request_and_parse(
-        fetch, "(spam := 'tasty')", session="bacon"
-    )
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert not response["output"]
-    assert response["result"] == ("'tasty'", "tasty")
+        response = await request(fetch, "(spam := 'tasty')", session="bacon")
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert not response["output"]
+        assert response["result"] == ("'tasty'", "tasty")
 
-    response = await request_and_parse(fetch, "spam", session="bacon")
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert not response["output"]
-    assert response["result"] == ("'tasty'", "tasty")
+        response = await request(fetch, "spam", session="bacon")
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert not response["output"]
+        assert response["result"] == ("'tasty'", "tasty")
 
-    response = await request_and_parse(fetch, "_", session="bacon")
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert not response["output"]
-    assert response["result"] == ("'tasty'", "tasty")
+        response = await request(fetch, "_", session="bacon")
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert not response["output"]
+        assert response["result"] == ("'tasty'", "tasty")
 
-    response = await request_and_parse(fetch, "print")
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert not response["output"]
-    assert isinstance(response["result"], tuple)
-    assert response["result"][1] is print
+        response = await request(fetch, "print")
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert not response["output"]
+        assert isinstance(response["result"], tuple)
+        assert response["result"][1] is print
 
-    response = await request_and_parse(fetch, "help")
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert not response["output"]
-    assert isinstance(response["result"], tuple)
-    assert type(response["result"][1]) is type(help)
+        response = await request(fetch, "help")
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert not response["output"]
+        assert isinstance(response["result"], tuple)
+        assert type(response["result"][1]) is type(help)
 
-    response = await request_and_parse(
-        fetch, "(await self.load_session())['__name__']"
-    )
-    assert isinstance(response, dict)
-    assert response["success"]
-    assert not response["output"]
-    assert response["result"] == ("'this'", "this")
+        response = await request(
+            fetch, "(await self.load_session())['__name__']"
+        )
+        assert isinstance(response, dict)
+        assert response["success"]
+        assert not response["output"]
+        assert response["result"] == ("'this'", "this")
 
-    response = await request_and_parse(fetch, "_")
-    assert isinstance(response, dict)
-    assert not response["success"]
-    assert not response["output"]
-    assert isinstance(response["result"], tuple)
-    assert response["result"][0].startswith("Traceback (most recent call last)")
-    assert isinstance(response["result"][1], NameError)
+        response = await request(fetch, "_")
+        assert isinstance(response, dict)
+        assert not response["success"]
+        assert not response["output"]
+        assert isinstance(response["result"], tuple)
+        assert response["result"][0].startswith(
+            "Traceback (most recent call last)"
+        )
+        assert isinstance(response["result"][1], NameError)
 
-    response = await request_and_parse(
-        fetch, "raise SystemExit('spam', 'eggs')", mode="exec"
-    )
-    assert isinstance(response, dict)
-    assert response["success"] is ...
-    assert not response["output"]
-    assert isinstance(response["result"], SystemExit)
-    assert response["result"].args == ("spam", "eggs")
+        response = await request(
+            fetch, "raise SystemExit('spam', 'eggs')", mode="exec"
+        )
+        assert isinstance(response, dict)
+        assert response["success"] is ...
+        assert not response["output"]
+        assert isinstance(response["result"], SystemExit)
+        assert response["result"].args == ("spam", "eggs")
 
-    # # create something that cannot be pickled
-    # response = await request_and_parse(
-    #     fetch,
-    #     "def toast():\n"
-    #     "   class Toast: pass\n"
-    #     "   return Toast\n"
-    #     "LocalToast = toast()\n"
-    #     "t = LocalToast()",
-    #     mode="exec",
-    #     session="sausage",
-    # )
-    # assert isinstance(response, dict)
-    # assert response["success"]
-    # assert not response["output"]
-    # assert response["result"] is None
+        # # create something that cannot be pickled
+        # response = await request(
+        #     fetch,
+        #     "def toast():\n"
+        #     "   class Toast: pass\n"
+        #     "   return Toast\n"
+        #     "LocalToast = toast()\n"
+        #     "t = LocalToast()",
+        #     mode="exec",
+        #     session="sausage",
+        # )
+        # assert isinstance(response, dict)
+        # assert response["success"]
+        # assert not response["output"]
+        # assert response["result"] is None
 
-    # response = await request_and_parse(
-    #     fetch, "raise SystemExit(t)", mode="exec", session="sausage"
-    # )
-    # assert isinstance(response, dict)
-    # assert response["success"] is ...
-    # assert not response["output"]
-    # assert isinstance(response["result"], SystemExit)
-    # assert (
-    #     response["result"]
-    #     .args[0]
-    #     .startswith("<this.toast.<locals>.Toast object at ")
-    # )
-    # assert response["result"].args[0].endswith(">")
+        # response = await request(
+        #     fetch, "raise SystemExit(t)", mode="exec", session="sausage"
+        # )
+        # assert isinstance(response, dict)
+        # assert response["success"] is ...
+        # assert not response["output"]
+        # assert isinstance(response["result"], SystemExit)
+        # assert (
+        #     response["result"]
+        #     .args[0]
+        #     .startswith("<this.toast.<locals>.Toast object at ")
+        # )
+        # assert response["result"].args[0].endswith(">")
 
-    # response = await request_and_parse(fetch, "t", session="sausage")
-    # assert isinstance(response, dict)
-    # assert response["success"]
-    # assert not response["output"]
-    # assert response["result"][0].startswith(
-    #     "<this.toast.<locals>.Toast object at "
-    # )
-    # assert response["result"][0].endswith(">")
-    # assert response["result"][1] is None
+        # response = await request(fetch, "t", session="sausage")
+        # assert isinstance(response, dict)
+        # assert response["success"]
+        # assert not response["output"]
+        # assert response["result"][0].startswith(
+        #     "<this.toast.<locals>.Toast object at "
+        # )
+        # assert response["result"][0].endswith(">")
+        # assert response["result"][1] is None
 
-    response = await request_and_parse(fetch, "0 / 0", mode="exec")
-    assert isinstance(response, dict)
-    assert not response["success"]
-    assert not response["output"]
-    assert isinstance(response["result"], tuple)
-    assert response["result"][0].startswith("Traceback (most recent call last)")
-    assert response["result"][1].args == ("division by zero",)
-    assert isinstance(response["result"][1], ZeroDivisionError)
+        response = await request(fetch, "0 / 0", mode="exec")
+        assert isinstance(response, dict)
+        assert not response["success"]
+        assert not response["output"]
+        assert isinstance(response["result"], tuple)
+        assert response["result"][0].startswith(
+            "Traceback (most recent call last)"
+        )
+        assert response["result"][1].args == ("division by zero",)
+        assert isinstance(response["result"][1], ZeroDivisionError)
