dfbf71ce10b7ece41b20349f71b24a4fe6a6b6ad
Joshix <joshix.dev@mailbox.org>
2025-02-13 20:00:00 +0000

    🫕 font-display

--- an_website/static/js/uptime/uptime.js
+++ an_website/static/js/uptime/uptime.js
@@ -1,4 +1,4 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0-or-later
-function s(){let e=document.getElementById("uptime");e.style.fontFamily="'clock-face', monospace";let a=performance.now()/1e3-parseFloat(e.getAttribute("uptime")),o=t=>String(Math.floor(t)).padStart(2,"0"),n=()=>{let t=Math.floor(performance.now()/1e3-a),i=Math.floor(t/60),r=Math.floor(i/60);e.innerText=[o(r/24),o(r%24),o(i%60),o(t%60)].join(":"),e.setAttribute("uptime",String(t))};n(),setInterval(n,1e3)}s();
+var o="uptime",m=()=>performance.now()/1e3,e=Math.floor,p=()=>{let t=document.getElementById(o);t.style.fontFamily="clock-face,monospace";let r=m()-Number(t.getAttribute(o)),s=()=>{let n=e(m()-r),c=e(n/60),i=e(c/60);t.innerText=[i/24,i%24,c%60,n%60].map(a=>(""+e(a)).padStart(2,"0")).join(":"),t.setAttribute(o,n)};s(),setInterval(s,1e3)};p();
 // @license-end
 //# sourceMappingURL=uptime.js.map
--- an_website/uptime/uptime.ts
+++ an_website/uptime/uptime.ts
@@ -1,35 +1,40 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0-or-later
 export {};
 
-function startDisplayingUptime() {
-    const uptimeDiv = document.getElementById("uptime") as HTMLDivElement;
-    // set the font family, because it looks much better
-    uptimeDiv.style.fontFamily = "'clock-face', monospace";
-    // the time the website got loaded minus the uptime
-    const startTime = (performance.now() / 1000) - parseFloat(
-        uptimeDiv.getAttribute("uptime")!,
-    );
+const UPTIME_STRING = "uptime" as const;
 
-    const zeroPad = (n: number) => String(Math.floor(n)).padStart(2, "0");
+const currentTimeInSeconds = () => (performance.now() / 1000);
+const floor = Math.floor;
+
+const startDisplayingUptime = () => {
+    const uptimeDiv = document.getElementById(UPTIME_STRING) as HTMLDivElement;
+    // set the font family, because it looks much better
+    uptimeDiv.style.fontFamily = "clock-face,monospace";
+    // the time the website got loaded minus the uptime
+    const startTime = currentTimeInSeconds() - Number(
+        uptimeDiv.getAttribute(UPTIME_STRING)!,
+    );
 
     const displayUptime = () => {
         // the uptime: currentTime - (startTime - uptime at start)
         //             time on website          + uptime at start
-        const uptime = Math.floor((performance.now() / 1000) - startTime);
+        const uptime = floor(currentTimeInSeconds() - startTime);
         // display uptime:
-        const div_60 = Math.floor(uptime / 60);
-        const div_60_60 = Math.floor(div_60 / 60);
+        const div_60 = floor(uptime / 60);
+        const div_60_60 = floor(div_60 / 60);
         uptimeDiv.innerText = [
-            zeroPad(div_60_60 / 24), // days
-            zeroPad(div_60_60 % 24), // hours
-            zeroPad(div_60 % 60), // minutes
-            zeroPad(uptime % 60), // seconds
+            div_60_60 / 24, // days
+            div_60_60 % 24, // hours
+            div_60 % 60, // minutes
+            uptime % 60, // seconds
         ]
+            .map((n: number) => ("" + floor(n)).padStart(2, "0"))
             .join(":");
-        uptimeDiv.setAttribute("uptime", String(uptime));
+        uptimeDiv.setAttribute(UPTIME_STRING, uptime);
     };
 
     displayUptime();
     setInterval(displayUptime, 1000);
-}
+};
+
 startDisplayingUptime();
