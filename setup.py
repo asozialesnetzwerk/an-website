b93cd1a4fec18137261f99d751837d2b4bc9c99c
Joshix <joshix.dev@mailbox.org>
2025-04-23 17:00:00 +0000

    🤦

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ if EGGINFO and path("pip-constraints.txt").exists():
                 for line in path("pip-dev-requirements.txt")
                 .read_text("UTF-8")
                 .splitlines()
-                if line.startswith(("pytest", "html5lib==", "time-machine=="))
+                if line.startswith(("pytest", "html5lib==", "time-machine==", "zstd=="))
             )
         )
         + "\n",
