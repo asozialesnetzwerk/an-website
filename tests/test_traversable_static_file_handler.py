038f1c2ec937e7bd7c30526a6fb3e352cde7a89d
Joshix <joshix.dev@mailbox.org>
2025-04-25 11:00:00 +0000

    🧪 improve test coverage of TraversableStaticFileHandler

--- tests/test_traversable_static_file_handler.py
+++ tests/test_traversable_static_file_handler.py
@@ -17,6 +17,11 @@ from __future__ import annotations
 
 import gzip
 
+import zstandard
+
+from an_website import DIR as ROOT_DIR
+from an_website.utils.fix_static_path_impl import recurse_directory
+
 from . import (  # noqa: F401  # pylint: disable=unused-import
     FetchCallable,
     app,
@@ -24,6 +29,7 @@ from . import (  # noqa: F401  # pylint: disable=unused-import
     fetch,
 )
 
+STATIC_DIR = ROOT_DIR / "static"
 CACHE_CONTROL = f"public, immutable, max-age={86400 * 365 * 10}"
 
 
@@ -47,6 +53,10 @@ async def test_openmoji(fetch: FetchCallable) -> None:  # noqa: F811
         assert int(compressed_response.headers["Content-Length"]) == size_compre
         if encoding == "gzip":
             assert response.body == gzip.decompress(compressed_response.body)
+        if encoding == "zstd":
+            assert response.body == zstandard.decompress(
+                compressed_response.body
+            )
 
     part_response = assert_valid_response(
         await fetch(
@@ -106,3 +116,127 @@ async def test_openmoji(fetch: FetchCallable) -> None:  # noqa: F811
     )
     assert int(part_response.headers["Content-Length"]) == 7
     assert part_response.body == b"</svg>\n"
+
+
+async def test_static_file_compression(
+    fetch: FetchCallable,  # noqa: F811
+) -> None:
+    """Test fetching static files."""
+    file_count, gzip_count, zstd_count = 0, 0, 0
+
+    for file in recurse_directory(
+        STATIC_DIR,
+        filter=lambda p: p.is_file() and not p.name.endswith((".zst", ".gz")),
+    ):
+        gzip_file = (STATIC_DIR / f"{file}.gz").is_file()
+        zstd_file = (STATIC_DIR / f"{file}.zst").is_file()
+
+        file_count += 1
+        gzip_count += gzip_file
+        zstd_count += zstd_file
+
+        uncompressed_body = b""
+
+        for encoding in ("identity", "gzip", "zstd"):
+            headers = {"Accept-Encoding": encoding}
+            response = await fetch(f"/static/{file}", headers=headers)
+            body = response.body
+            assert response.code == 200
+            assert response.headers["Content-Length"] == str(len(body))
+
+            head_response = await fetch(
+                f"/static/{file}", headers=headers, method="HEAD"
+            )
+            assert head_response.code == 200
+            assert head_response.headers["Content-Length"] == str(len(body))
+
+            headers = dict(response.headers)
+            head_headers = dict(head_response.headers)
+            del head_headers["Date"], headers["Date"]
+            assert head_headers == headers, f"{head_headers} != {headers}"
+
+            if encoding == "identity":
+                assert "Content-Encoding" not in response.headers
+                uncompressed_body = body
+                continue
+            if "Content-Encoding" not in response.headers:
+                if encoding == "gzip":
+                    assert not gzip_file
+                elif encoding == "zstd":
+                    assert not zstd_file
+                else:
+                    raise AssertionError(f"Unknown encoding {encoding}")
+                continue
+
+            assert response.headers.get("Content-Encoding") == encoding
+
+            body = response.body
+            assert len(body) < len(uncompressed_body)
+
+            decompressed_body = b""
+            if encoding == "gzip":
+                assert gzip_file
+                decompressed_body = gzip.decompress(body)
+            elif encoding == "zstd":
+                assert zstd_file
+                decompressed_body = zstandard.decompress(body)
+            else:
+                raise AssertionError(f"Unknown encoding {encoding}")
+
+            assert decompressed_body
+            assert uncompressed_body == decompressed_body
+
+    assert file_count >= gzip_count > 0
+    assert file_count >= zstd_count > 0
+
+
+async def test_invalid_paths(
+    fetch: FetchCallable,  # noqa: F811
+) -> None:
+    """Test various different invalid paths."""
+    response = await fetch("/static/humans.txt/")
+    assert response.code == 307
+    assert response.headers["location"].endswith("/static/humans.txt")
+
+    response = await fetch("/static//humans.txt")
+    assert response.code == 404
+
+    response = await fetch("/static/INVALID_FILE")
+    assert response.code == 404
+
+    response = await fetch("/static/HUMANS.TXT")
+    assert response.code == 307
+    assert response.headers["location"].endswith("/static/humans.txt")
+
+
+async def test_invalid_range(
+    fetch: FetchCallable,  # noqa: F811
+) -> None:
+    """Test invalid range requests."""
+    for encoding in ("identity", "gzip", "zstd"):
+        headers = {"Accept-Encoding": encoding}
+
+        response = await fetch("/static/robots.txt", headers=headers)
+        size = len(response.body)
+
+        response = await fetch(
+            "/static/robots.txt", headers={**headers, "Range": f"bytes={size}"}
+        )
+        assert response.code == 416
+        assert response.headers["Content-Type"] == "text/plain"
+        assert response.headers["Content-Range"] == f"bytes */{size}"
+
+        response = await fetch(
+            "/static/robots.txt",
+            headers={**headers, "Range": f"bytes={size * 2 + 100}"},
+        )
+        assert response.code == 416
+        assert response.headers["Content-Type"] == "text/plain"
+        assert response.headers["Content-Range"] == f"bytes */{size}"
+
+        response = await fetch(
+            "/static/robots.txt", headers={**headers, "Range": "bytes=-0"}
+        )
+        assert response.code == 416
+        assert response.headers["Content-Type"] == "text/plain"
+        assert response.headers["Content-Range"] == f"bytes */{size}"
