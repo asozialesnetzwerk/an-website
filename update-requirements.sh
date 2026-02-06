3f6c2a8e17e69b9421a5e598970731faea7535af
Joshix <joshix.dev@mailbox.org>
2026-02-06 22:00:00 +0000

    Revert "➰ update pycurl"
    This reverts commit 65e158b018998ca75c67c0f2b1a5d2cec8d3d75f.

--- update-requirements.sh
+++ update-requirements.sh
@@ -14,7 +14,3 @@ sed -i "/^-/d" pip-dev-requirements.txt
 sed -i "/^-/d" pip-constraints.txt
 
 sed -i "s/\[.*\]//" pip-constraints.txt
-
-PYCURL_VERSION="$(python -c 'print(*[l.split(";")[0].split("==")[1] for l in open("pip-requirements.txt") if l.startswith("pycurl==")])')"
-PY_CURL_SDIST_URL="$(curl -sSf https://pypi.org/pypi/pycurl/json | jq -r '.releases["'"${PYCURL_VERSION}"'"][] | select(.packagetype == "sdist") | .url')"
-sed -i "s|^pycurl==${PYCURL_VERSION}|pycurl @ ${PY_CURL_SDIST_URL} |" pip-requirements.txt pip-dev-requirements.txt
